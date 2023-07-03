c4a177f86b47a734f2da8370aa4950b3cae45261
许圣峰 <kricsleo@163.com>
2023-07-03 16:12:19 +0800

    feat: calculate tokens

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
-# gpt-token
+<h1 align="center">gpt-token</h1>
+<p align="center">
+<a href="https://www.npmjs.com/package/gpt-token">
+  <img src="https://img.shields.io/npm/v/gpt-token?style=flat&colorA=18181B&colorB=F0DB4F" />
+</a>
+
+<a href="https://www.npmjs.com/package/gpt-token">
+  <img src="https://img.shields.io/npm/types/gpt-token?style=flat&colorA=18181B&colorB=F0DB4F" />
+</a>
+
+<a href="https://bundlephobia.com/package/gpt-token">
+  <img src="https://img.shields.io/bundlephobia/minzip/gpt-token?style=flat&colorA=18181B&colorB=F0DB4F" />
+</a>
+
+<a href="https://github.com/kricsleo/gpt-token/blob/master/LICENSE">
+  <img src="https://img.shields.io/github/license/kricsleo/gpt-token.svg?style=flat&colorA=18181B&colorB=F0DB4F" />
+</a>
+
+</p>
+
+Calculate the number of text tokens in GPT.
+
+## Usage
+
+```bash
+npm i gpt-token
+```
+
+```ts
+import { calcTextTokens, calcMessageTokens } from 'gpt-token'
+
+// => 
+console.log(calcTextTokens('A quick brown fox jumps over the lazy dog'))
+
+console.log(calcMessageTokens([
+  {
+    role: 'user',
+    content: 'A quick brown fox jumps over the lazy dog'
+  }
+]))
+```
+
+## Thanks ❤️
+
+- [gpt-tokens](https://github.com/Cainier/gpt-tokens/tree/main)
+- [openai-cookbook](https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb)
 
 ## License
 
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,58 @@
-export const one = 1
-export const two = 2
+import { encodingForModel, TiktokenModel } from 'js-tiktoken'
+
+export interface Message {
+  name?: string
+  role: 'system' | 'user' | 'assistant'
+  content: string
+}
+
+export function calcTextTokens(text: string, model: TiktokenModel) {
+  const encoder = encodingForModel(model)
+  return encoder.encode(text).length
+}
+
+// https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
+export function calcMessageTokens(message: Message | Message[], model: TiktokenModel) {
+  let tokensPerMessage: number
+  let tokensPerName: number
+  const primeTokens = 3
+  if('gpt-3.5-turbo-0301' === model) {
+    tokensPerMessage = 4
+    tokensPerName = -1
+  } else if ([
+    'gpt-3.5-turbo',
+    'gpt-3.5-turbo-0613',
+    'gpt-3.5-turbo-16k',
+    'gpt-3.5-turbo-16k-0613',
+    'gpt-4',
+    'gpt-4-0314',
+    'gpt-4-0613',
+    'gpt-4-32k',
+    'gpt-4-32k-0314',
+    'gpt-4-32k-0613',
+  ].includes(model)) {
+    tokensPerMessage = 3
+    tokensPerName = 1
+  } else if(model.includes('gpt-3.5-turbo')) {
+    console.warn('Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.')
+    return calcMessageTokens(message, 'gpt-3.5-turbo')
+  } else if(model.includes('gpt-4')) {
+    console.warn('Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.')
+    return calcMessageTokens(message, 'gpt-4-0613')
+  } else {
+    throw new Error(`Unsupported model: ${model}`)
+  }
+  const encoder = encodingForModel(model)
+  const messages = Array.isArray(message) ? message : [message]
+  const tokens = messages.reduce((acc, msg) => {
+    acc += tokensPerMessage
+    for (const [key, value] of Object.entries(msg)) {
+      acc += encoder.encode(value).length
+      if (key === 'name') { 
+        acc += tokensPerName 
+      }
+    }
+    return acc
+  }, 0)
+  return tokens + primeTokens
+}
