92debcbbc9da8bb51b446e48437cf488e7a54fd4
许圣峰 <kricsleo@163.com>
2023-07-04 17:23:05 +0800

    chore: add playground

---
+++ playground/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { textTokens } from 'gpt-token'
+
+const text = 'A quick brown fox jumps over the lazy dog'
+const model = 'gpt-3.5-turbo-0301'
+</script>
+
+<template>
+  <div font-sans p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <main>
+      token size of '{{ text }}' in model '{{ model }}': {{ textTokens(text, model) }}
+    </main>
+  </div>
+</template>
